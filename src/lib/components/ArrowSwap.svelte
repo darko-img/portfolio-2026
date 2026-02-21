83328fe8131f3240488390eb8123aa51193b04f8
Dariusz Tomaszewski <darekimg@gmail.com>
2026-02-22 00:18:57 +0100

    footer links

--- src/lib/components/ArrowSwap.svelte
+++ src/lib/components/ArrowSwap.svelte
@@ -1,8 +1,8 @@
 <span class="relative w-5 h-5 overflow-hidden">
   <!-- sichtbar -->
   <span
-    class="absolute inset-0 flex items-center justify-center
-           transition-transform duration-400
+    class="text-[var(--color-off)] absolute inset-0 flex items-center justify-center
+           transition-transform duration-200
            ease-[cubic-bezier(0.4,0,0.2,1)]
            group-hover:-translate-y-full group-hover:translate-x-full">
     ↗
@@ -12,7 +12,7 @@
   <span
     class="absolute inset-0 flex items-center justify-center
            translate-y-full -translate-x-full
-           transition-transform duration-400
+           transition-transform duration-200
            ease-[cubic-bezier(0.4,0,0.2,1)]
            group-hover:translate-y-0 group-hover:translate-x-0">
     ↗
